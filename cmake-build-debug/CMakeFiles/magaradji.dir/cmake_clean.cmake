c61dd250f33180879cb162d890763d1b80be40bb
vladbusov <to2002to2002@gmail.com>
2022-01-28 05:37:45 +0300

    update

---
+++ cmake-build-debug/CMakeFiles/magaradji.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/magaradji.dir/main.cpp.o"
+  "libmagaradji.dll.a"
+  "magaradji.exe"
+  "magaradji.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/magaradji.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
